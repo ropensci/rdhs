6f439dea060e625526e3b37951e544593a6491b4
OJWatson <o.watson15@imperial.ac.uk>
2018-01-23 22:24:45 +0000

    fixed message testing pass

--- tests/testthat/test_downloads.R
+++ tests/testthat/test_downloads.R
@@ -2,7 +2,7 @@ context("Downloads")
 
 test_that("download", {
 
-
+  # test that
    dhs_surveys <- rdhs:::available_surveys(your_email="rdhs.tester@gmail.com",
    your_password="rdhstesting",
    your_project="Testing Malaria Investigations",
@@ -12,9 +12,12 @@ test_that("download", {
    testthat::expect_message(download_datasets(your_email="rdhs.tester@gmail.com",
    your_password="rdhstesting",
    your_project="Testing Malaria Investigations",
-   desired_surveys=dhs_surveys[1,]),paste0("DHS survey entry 1 of 1 stored in '",dhs_surveys[1,]$output_folder,"'"))
+   desired_surveys=dhs_surveys[1,]),paste0("\n\nDHS survey entry 1 of 1 stored in '",dhs_surveys[1,]$output_folder,"'\r\n\n\n"),fixed=TRUE)
 
 
 
 
 })
+
+"\n\nDHS survey entry 1 of 1 stored in 'C:\\Users\\Oliver\\AppData\\Local\\Temp\\Rtmp0i1UTJ/Angola/Standard DHS 2015-16'\r\n\n\n"
+"\n\nDHS survey entry 1 of 1 stored in 'C:\\Users\\Oliver\\AppData\\Local\\Temp\\Rtmp0i1UTJ/Angola/Standard DHS 2015-16'\r\n\n\n"
