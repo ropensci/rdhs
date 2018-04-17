d74fbdc00d0abbc717f5c586a16b08b92ad10c6a
OJWatson <o.watson15@imperial.ac.uk>
2018-04-17 12:40:58 +0100

    trying change 2/2

--- R/utils.R
+++ R/utils.R
@@ -82,3 +82,63 @@ is_uppercase <- function (string)
     string <- as.character(string)
   !grepl("[a-z]", string, perl = TRUE)
 }
+
+
+## helper functions - not package related - but easiest to have here while developing
+## ----------------------------------------------
+
+
+# call_tree
+match.call.defaults <- function(...) {
+  call <- evalq(match.call(expand.dots = FALSE), parent.frame(1))
+  formals <- evalq(formals(), parent.frame(1))
+
+  for(i in setdiff(names(formals), names(call)))
+    call[i] <- list( formals[[i]] )
+
+
+  match.call(sys.function(sys.parent()), call)
+}
+
+# open file outside'
+sopen <- function(txt_path) system(paste0("open ","\"",txt_path,"\""))
+
+# open folder outside
+sdir <- function(x)  {if(Sys.info()["sysname"] == "Windows") shell.exec(x) else system2("open", x)}
+
+# chracterise vector
+rechar_vec <- function(what) cat(paste0("c(\"",paste0(what,collapse="\",\""),"\")"))
+
+# itemise vector
+itemise_vec <- function(what) {
+  cat("##' \\itemize{\n")
+  invisible(sapply(what,function(x) paste0("##'       \\item{\"",x,"\"}{ }\n") %>% cat))
+  cat("##'       }")
+}
+
+# itemise data.frame of 2 attributes
+itemise_df2 <- function(what) {
+  paste0("##' \\itemize{\n",
+         paste0(apply(what,MARGIN = 1,function(x) paste0("##'       \\item{\"",x[1],"\"}{",paste0(x[2],collapse=""),"}\n") %>% paste0(collapse="")),collapse=""),
+         "##'       }")
+}
+
+# camelcase: Credit: http://www.stat.cmu.edu/~hseltman/files/camelCase.R
+camelCase <- function(sv, upper=FALSE, capIsNew=FALSE, alreadyTrimmed=FALSE) {
+  if (!is.character(sv)) stop("'sv' must be a string vector")
+  if (!alreadyTrimmed) sv <- gsub("[[:space:]]*$", "", gsub("^[[:space:]]*", "", sv))
+  if (capIsNew) {
+    sv <- gsub("([A-Z])", " \\1", sv)
+    sv <- gsub("^[[:space:]]", "", sv)
+    sv <- tolower(sv)
+  }
+  apart <- strsplit(sv, split="[[:space:][:punct:]]")
+  apart <- lapply(apart, tolower)
+  capitalize <- function(x) paste0(toupper(substring(x,1,1)), substring(x,2))
+  if (upper) {
+    apart <- lapply(apart, capitalize)
+  } else {
+    apart <- lapply(apart, function(x) c(x[1], capitalize(x[-1])))
+  }
+  return(sapply(apart, paste, collapse=""))
+}
