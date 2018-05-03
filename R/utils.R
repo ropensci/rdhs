3813325bdac49a84eb6299c6cd657bb03f5cbcf2
OJWatson <o.watson15@imperial.ac.uk>
2018-05-03 16:33:25 +0100

    seq_len is better than 1:

--- R/utils.R
+++ R/utils.R
@@ -17,7 +17,7 @@ rbind_list_base <- function(x) {
     rbind.data.frame,
     c(x, stringsAsFactors = FALSE, make.row.names = FALSE)
   )
-  rownames(x2) <- 1:(dim(x2)[1])
+  rownames(x2) <- seq_len(dim(x2)[1])
   x2
 }
 
--- tests/testthat/test_miscellaneous.R
+++ tests/testthat/test_miscellaneous.R
@@ -1,30 +1,34 @@
 context("Miscellaneous utils")
 
 test_that("file_dataset_format", {
+  dhs_file_formats <- c(
+    "Flat ASCII data (.dat)",
+    "Hierarchical ASCII data (.dat)",
+    "SAS dataset (.sas7bdat) ",
+    "SPSS dataset (.sav)",
+    "Stata dataset (.dta)"
+  )
 
-  dhs_file_formats <- c("Flat ASCII data (.dat)",
-                        "Hierarchical ASCII data (.dat)",
-                        "SAS dataset (.sas7bdat) ",
-                        "SPSS dataset (.sav)",
-                        "Stata dataset (.dta)")
-
-  expect_identical(file_dataset_format(dhs_file_formats[1]),"dat")
-  expect_identical(file_dataset_format(dhs_file_formats[2]),"dat")
-  expect_identical(file_dataset_format(dhs_file_formats[3]),"sas7bdat")
-  expect_identical(file_dataset_format(dhs_file_formats[4]),"sav")
-  expect_identical(file_dataset_format(dhs_file_formats[5]),"dta")
-
+  expect_identical(file_dataset_format(dhs_file_formats[1]), "dat")
+  expect_identical(file_dataset_format(dhs_file_formats[2]), "dat")
+  expect_identical(file_dataset_format(dhs_file_formats[3]), "sas7bdat")
+  expect_identical(file_dataset_format(dhs_file_formats[4]), "sav")
+  expect_identical(file_dataset_format(dhs_file_formats[5]), "dta")
 })
 
 # test the base version of rbindlist from data.table implemented for our uses
 test_that("rbind_list_base", {
-
   l <- list()
-  l[[1]] <- list("a"=1,"b"=2,"c"=3)
-  l[[2]] <- list("a"=1,"b"=2,"c"=3)
+  l[[1]] <- list("a" = 1, "b" = 2, "c" = 3)
+  l[[2]] <- list("a" = 1, "b" = 2, "c" = 3)
   l <- rbind_list_base(l)
 
-  expect_equal(dim(l),c(2,3))
-  expect_equal(names(l),c("a","b","c"))
-  expect_equal(l$a,c(1,1))
+  expect_equal(dim(l), c(2, 3))
+  expect_equal(names(l), c("a", "b", "c"))
+  expect_equal(l$a, c(1, 1))
+
+  l <- list()
+  l[[1]] <- list()
+  l <- rbind_list_base(l)
+
 })
